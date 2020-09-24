a934deb8d102cc3dd2364d1ba8c74a2c51a8fc62
Stefan Novasky <stefannovasky@gmail.com>
2020-09-24 15:58:17 -0300

    learning about dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Date from '../components/Date'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
